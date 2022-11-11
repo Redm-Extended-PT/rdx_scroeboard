65926be90949fb7332e0f909e2e2ce1838bb2b5a
Nevermind <58900773+RalivTV@users.noreply.github.com>
2022-11-11 13:24:10 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'adamant'
 games {'rdr3'}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'K1Dev'
+author 'RDX'
 version '1.0'
 
 server_scripts {
